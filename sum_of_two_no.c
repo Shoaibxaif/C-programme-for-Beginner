adaf72e228db66805f03457f4be18fb0e7d23596
mohammad shoaib <mohammadshoaib7221@gmail.com>
2022-10-29 21:17:27 +0530

    new added

---
+++ sum_of_two_no.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2,sum;
+
+    printf("Enter the value of n1:\n");
+    scanf("%d",&n1);
+
+    printf("Enter the value of n2:\n");
+    scanf("%d",&n2);
+
+    sum = n1+n2;
+    printf("addition of Two numbers:%d",sum);
+
+    return 0;
+}
