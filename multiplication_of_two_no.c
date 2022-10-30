3491f67727c5d4969432700db86edd255a067a12
mohammad shoaib <mohammadshoaib7221@gmail.com>
2022-10-30 23:42:00 +0530

    multiplication of 2 no.

---
+++ multiplication_of_two_no.c
@@ -0,0 +1,19 @@
+
+
+// Multiplication Of two Numbers
+
+#include<stdio.h>
+int main()
+{
+     int n1,n2,mul;
+
+    printf("Enter the value of n1:\n");
+    scanf("%d",&n1);
+
+    printf("Enter the value of n2:\n");
+    scanf("%d",&n2);
+
+    mul = n1*n2;
+    printf("Multiplication of Two numbers:%d",mul);
+    return 0;
+}
