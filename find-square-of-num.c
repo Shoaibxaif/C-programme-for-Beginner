41590c51038ede2e4eb6f9c1731b1055bd16c2d1
Mohammad Shoaib <91245904+Shoaibxaif@users.noreply.github.com>
2022-11-13 06:15:34 +0000

    upload

---
+++ find-square-of-num.c
@@ -0,0 +1,11 @@
+// FIND SQUARE OF A NUMBER
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter No.\n");
+    scanf("%d",&n);
+
+    printf("The square of %d = %d",n,n*n);
+    return 0;
+}
