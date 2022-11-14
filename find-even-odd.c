c8b8a35509d54bccc202a58cbe2059ee40307fae
mohammad shoaib <mohammadshoaib7221@gmail.com>
2022-11-15 01:19:11 +0530

    upload

---
+++ find-even-odd.c
@@ -0,0 +1,25 @@
+// Find even or odd
+
+#include<stdio.h>
+int main()
+{
+    int n;
+
+    printf("Enter the value of n:\n");
+    scanf("%d", &n);
+
+    if (n%2==0)
+    {
+        printf("%d is even",n);
+    }
+
+    else
+    {
+        printf("%d is odd",n);
+    }
+    
+   
+
+
+    return 0;
+}
